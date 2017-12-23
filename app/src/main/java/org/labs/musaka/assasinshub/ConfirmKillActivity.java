329361236a1a8323cc62de233ac25bf93618b074
rimogardino <tiddley@abv.bg>
2017-12-23 18:32:56 +0200

    Change this god dammit!!

--- app/src/main/java/org/labs/musaka/assasinshub/ConfirmKillActivity.java
+++ app/src/main/java/org/labs/musaka/assasinshub/ConfirmKillActivity.java
@@ -1,13 +1,52 @@
 package org.labs.musaka.assasinshub;
 
+
+import android.os.Build;
+import android.os.Handler;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.cunoraz.gifview.library.GifView;
+
 
 public class ConfirmKillActivity extends AppCompatActivity {
+    private final int interval = 3000; // 3 Second
+    private Handler handler = new Handler();
+    private Runnable runnable;
+    private GifView killingGif;
+    private TextView confirmation;
 
+
+
+
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_confirm_kill);
+
+        confirmation = (TextView) findViewById(R.id.tv_killConfirmed);
+        confirmation.setVisibility(View.INVISIBLE);
+
+        killingGif = (GifView) findViewById(R.id.iv_killinggif);
+        killingGif.setGifResource(R.drawable.kill_confirmed);
+        killingGif.play();
+
+
+        runnable = new Runnable(){
+            public void run() {
+                killingGif.setVisibility(View.INVISIBLE);
+                confirmation.setVisibility(View.VISIBLE);
+            }
+        };
+
+        handler.postAtTime(runnable, System.currentTimeMillis()+interval);
+        handler.postDelayed(runnable, interval);
     }
+
 }
--- app/src/main/java/org/labs/musaka/assasinshub/MainActivity.java
+++ app/src/main/java/org/labs/musaka/assasinshub/MainActivity.java
@@ -1,17 +1,43 @@
 package org.labs.musaka.assasinshub;
 
+import android.animation.ObjectAnimator;
+import android.app.ActionBar;
 import android.content.Intent;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private Button button1;
+    private Button button2;
+    private Button button3;
+    private Button button4;
+    private Button[] buttons;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button1 = (Button) findViewById(R.id.b_bored);
+        button2 = (Button) findViewById(R.id.b_call);
+        button3 = (Button) findViewById(R.id.b_confirm_kill);
+        button4 = (Button) findViewById(R.id.b_leap);
+        buttons = new Button[]{button1, button2, button3, button4};
+
+        for (int i=0;i<buttons.length;i++) {
+            shrinkMe(buttons[i]);
+
+        }
+
+        for (int i=0;i<buttons.length;i++) {
+
+            shrinker(buttons[i]);
+        }
     }
 
     public void confirmKill(View view) {
@@ -35,5 +61,20 @@ public class MainActivity extends AppCompatActivity {
     public void imBored(View view) {
         Intent imBoredIntent = new Intent(this,ImBoredActivity.class);
         startActivity(imBoredIntent);
+    }
+
+    public void shrinker(View view) {
+        ObjectAnimator animationShrink = ObjectAnimator.ofFloat(view,
+                "ScaleX", 1);
+        animationShrink.setDuration(1700);
+        animationShrink.start();
+    }
+
+    public void shrinkMe(View view) {
+        ObjectAnimator animationShrink = ObjectAnimator.ofFloat(view,
+                "ScaleX", 0);
+        animationShrink.setDuration(0);
+        animationShrink.start();
+
     }
 }
